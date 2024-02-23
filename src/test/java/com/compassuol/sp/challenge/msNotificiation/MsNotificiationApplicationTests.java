498134ed57167aed38886f9a1ae16b66d0e3c26f
Vinicius Guerra <itsviniciusguerra@gmail.com>
2024-02-23 16:45:27 -0300

    first iteration

---
+++ src/test/java/com/compassuol/sp/challenge/msNotificiation/MsNotificiationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.compassuol.sp.challenge.msNotificiation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsNotificiationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
