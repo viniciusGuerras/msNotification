1f7f24ee662d568d4b0576781a4a902bd360bb82
Vinicius Guerra <itsviniciusguerra@gmail.com>
2024-02-25 01:11:24 -0300

    refactor: renamed the main folder

--- src/main/java/com/compassuol/sp/challenge/msNotificiation/domain/repository/NotificationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.compassuol.sp.challenge.msNotificiation.domain.repository;
-
-import com.compassuol.sp.challenge.msNotificiation.domain.entity.NotificationLog;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface NotificationRepository extends MongoRepository<NotificationLog, Long> {
-}
